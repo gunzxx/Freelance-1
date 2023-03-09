d22b2cf10e2fb8f23149ed822176d9df84503016
gunzxx <ggm5850@gmail.com>
2023-03-09 20:08:04 +0700

    client

--- 01 Titan/assets/js/script.js
+++ 01 Titan/assets/js/script.js
@@ -1,4 +1,4 @@
 const phoneSize = window.matchMedia("(max-width: 398px)");
 if(phoneSize.matches){
-    $("#mechanismeImg").attr('src','assets/img/mechanisme-h.png') ;
+    $("#mechanismeImg").attr('src','./assets/img/mechanisme-h.png') ;
 }
