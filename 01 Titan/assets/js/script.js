b6656702a6831071735824c013776aeb936f298a
gunzxx <ggm5850@gmail.com>
2023-03-10 17:11:51 +0700

    responsive navbar

--- 01 Titan/assets/js/script.js
+++ 01 Titan/assets/js/script.js
@@ -2,3 +2,18 @@ const phoneSize = window.matchMedia("(max-width: 398px)");
 if(phoneSize.matches){
     $("#mechanismeImg").attr('src','./assets/img/mechanisme-h.png') ;
 }
+
+let tutup = true;
+$('#toggle-nav').on('click',()=>{
+    if(tutup==true){
+        $('#sub1').css('display','flex');
+        $('#sub-nav').css('position','relative');
+        $('#sub-nav').css('top','0');
+        tutup = false;
+    }else{
+        $('#sub1').css('display', 'none');
+        $('#sub-nav').css('position', 'absolute');
+        $('#sub-nav').css('top', '-200px');
+        tutup = true;
+    }
+});
